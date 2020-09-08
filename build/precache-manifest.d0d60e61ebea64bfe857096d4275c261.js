4ee77cb6dbd9f59cfad66ebdd9c3f95ccd602fbd
saurav188 <saurav188@users.github.com>
2020-09-08 14:29:16 +0545

    removed build from gitignore

---
+++ build/precache-manifest.d0d60e61ebea64bfe857096d4275c261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e1d495e946658f402c999cfedcf460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08aab6deb03eb41c0c6",
+    "url": "/static/css/main.76be8afb.chunk.css"
+  },
+  {
+    "revision": "d79b2af5b98261bf7cc5",
+    "url": "/static/js/2.38635d70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38635d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08aab6deb03eb41c0c6",
+    "url": "/static/js/main.313d6fbf.chunk.js"
+  },
+  {
+    "revision": "cec9f6c4f6ff3433a235",
+    "url": "/static/js/runtime-main.e75335f0.js"
+  }
+]);
